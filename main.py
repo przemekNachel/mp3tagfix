6bb7f34ffc7491cd83d25e422220a8a08624eaac
przemekNachel <przemeknachel@gmail.com>
2020-09-29 22:52:13 +0200

    Just start

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import taglib
 
-for root, dirs, files in os.walk("D:\\mp3"):
+for root, dirs, files in os.walk("D:\\P\\Lamus\\mp3\\Albumy\\"):
+    album = []
     for file in files:
         if file.endswith(".mp3"):
             song = taglib.File(os.path.join(root, file))
-            print(song.tags["ALBUM"])
+            alb = song.tags.get("ALBUM")
+            if alb:
+                album.extend(alb)
+    album = list(set(album))
+    if len(album) > 1:
+        print(album)
